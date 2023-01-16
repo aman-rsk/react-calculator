e50ec1cb7041ce7107d885f888660f10c6f838da
aman-rsk <115797086+aman-rsk@users.noreply.github.com>
2023-01-16 14:08:32 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [calc, setCalc] = useState("");
+	const [result, setResult] = useState("");
+
+	const updateCalc = (value) => {
+		if (
+			(operation.includes(value) && calc === "") ||
+			(operation.includes(value) && operation.includes(calc.slice(-1)))
+		) {
+			return;
+		}
+		setCalc(calc + value);
+
+		if (!operation.includes(value)) {
+			setResult(eval(calc + value).toString());
+		}
+	};
+
+	const operation = ["+", "-", "*", "/", "."];
+
+	const calculate = () => {
+		setCalc(eval(calc).toString());
+	};
+
+	const delDigit = () => {
+		if (calc == "0") {
+			return;
+		}
+
+		const value = calc.slice(0, -1);
+		setCalc(value);
+	};
+
+	const allClear = () => {
+		setCalc("");
+		setResult(0);
+	};
+
+	return (
+		<div className="calculator-grid">
+			<div className="output">
+				<div className="previous-operand">
+					{result ? <span>{result}</span> : ""}
+				</div>
+				<div className="current-operand">{calc || "0"}</div>
+			</div>
+			<button className="span-two" onClick={allClear}>
+				AC
+			</button>
+			<button onClick={delDigit}>DEL</button>
+			<button onClick={() => updateCalc("/")}>/</button>
+			<button onClick={() => updateCalc("1")}>1</button>
+			<button onClick={() => updateCalc("2")}>2</button>
+			<button onClick={() => updateCalc("3")}>3</button>
+			<button onClick={() => updateCalc("*")}>*</button>
+			<button onClick={() => updateCalc("4")}>4</button>
+			<button onClick={() => updateCalc("5")}>5</button>
+			<button onClick={() => updateCalc("6")}>6</button>
+			<button onClick={() => updateCalc("+")}>+</button>
+			<button onClick={() => updateCalc("7")}>7</button>
+			<button onClick={() => updateCalc("8")}>8</button>
+			<button onClick={() => updateCalc("9")}>9</button>
+			<button onClick={() => updateCalc("-")}>-</button>
+			<button className="fullStop" onClick={() => updateCalc(".")}>
+				.
+			</button>
+			<button onClick={() => updateCalc("0")}>0</button>
+			<button className="span-two equal" onClick={calculate}>
+				=
+			</button>
+		</div>
+	);
 }
 
 export default App;
